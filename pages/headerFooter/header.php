647d34e43c471dc56d5a8918224ff05431797302
Tygo00 <tygodiks@gmail.com>
2026-05-18 16:47:00 +0200

    Start header
    Made start with header

--- pages/headerFooter/header.php
+++ pages/headerFooter/header.php
@@ -1,15 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <link rel="stylesheet" href="../../assets/css/headerCss.css">
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-    </head>
-    <body>
-        <header>
-            <nav>
-                <img src="assets/img/OGTOL.png" alt="" height="100vw" width="20%">
-            </nav>
-        </header>
-    </body>
+
+<head>
+    <link rel="stylesheet" href="../../assets/css/headerCss.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <header>
+        <nav>
+            <div class="headerImage">
+                <img src="assets/img/OGTOL.png" alt="" height="80vw" width="30%">
+            </div>
+            <div class="headerNav">
+                <a href="#home">Home</a>
+                <a href="#book">Home</a>
+                <a href="#about">About us</a>
+                <a href="#contact">Contact</a>
+            </div>
+            <div class="headerProfile">
+                <img src="" alt="">
+            </div>
+        </nav>
+    </header>
+</body>

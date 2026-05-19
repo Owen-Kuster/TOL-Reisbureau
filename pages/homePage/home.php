71a90aed568378d633738d6cfa7a185e18107ecd
Tygo00 <tygodiks@gmail.com>
2026-05-19 12:14:40 +0200

    home page start
    started with home page almost finished container 1

--- pages/homePage/home.php
+++ pages/homePage/home.php
@@ -1,19 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="../../assets/css/home.css">
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
+        <title>TOL-Home</title>
     </head>
     <body>
-        <header>
-            <nav>
-
-            </nav>
-        </header>
         <main>
+            <div class="container1">
+                <img src="../../assets/img/homeScreenImg.png" alt="" width="100%">
+                <div class="search-box">
 
+                </div>
+            </div>
+
+            <div class="cointainer2 test">
+                
+            </div>
         </main>
         <footer>
             

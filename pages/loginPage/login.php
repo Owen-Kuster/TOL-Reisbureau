514584c017c9896e31ff99c167d6be61984e8fe5
LauraPos <1214217@student.roc-nijmegen.nl>
2026-05-19 12:13:20 +0200

    Starting LoginPage
    HTML/CSS login page start

---
+++ pages/loginPage/login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../../assets/css/login.css">
+</head>
+<header>
+    <?php
+    include ('../headerFooter/header.php');
+    ?>
+</header>
+<body>
+    <Section class="flex-center">
+        <div class="bubble">
+        <h1>Login</h1>
+        </div>
+    </Section>
+</body>
+<footer>
+    <?php
+    include '../headerFooter/footer.php';
+    ?>
+</footer>
+</html>

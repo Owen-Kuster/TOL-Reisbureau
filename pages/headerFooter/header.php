4e6d18bed29c0a7dd70772c2237018be4dae1dbc
Owen Kuster <owenkuster08@gmail.com>
2026-05-19 11:26:16 +0200

    Header fix 2.0

--- pages/headerFooter/header.php
+++ pages/headerFooter/header.php
@@ -1,16 +1,17 @@
+<link rel="stylesheet" href="../../assets/css/headerCss.css">
 <header>
-        <nav>
-            <a href="#home" class="headerImage">
-                <img src="../../assets/img/OGTOL.png" alt="OGTOLLogo" height="75vw" width="30%">
-            </a>
-            <div class="headerNav">
-                <a href="#home">Home</a>
-                <a href="#book">Book</a>
-                <a href="#about">About us</a>
-                <a href="#contact">Contact</a>
-            </div>
-            <div class="headerProfile">
-                <img src="../../assets/img/ProfilePictureNoLogin.png" alt="profilePicture" height="30vw" width="30%">
-            </div>
-        </nav>
-    </header>
+    <nav>
+        <a href="#home" class="headerImage">
+            <img src="../../assets/img/OGTOL.png" alt="OGTOLLogo" height="75vw" width="30%">
+        </a>
+        <div class="headerNav">
+            <a href="#home">Home</a>
+            <a href="#book">Book</a>
+            <a href="#about">About us</a>
+            <a href="#contact">Contact</a>
+        </div>
+        <div class="headerProfile">
+            <img src="../../../assets/img/ProfilePictureNoLogin.png" alt="profilePicture" height="30vw" width="30%">
+        </div>
+    </nav>
+</header>

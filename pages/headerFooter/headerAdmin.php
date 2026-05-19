41d2d2fcdffb3fbabd92dc1ded5146e652555cbd
Owen Kuster <owenkuster08@gmail.com>
2026-05-19 12:14:21 +0200

    HeaderAdmin v1.0
    Working on the admin header, added css. added adminheader php. Working on css for the logout.

---
+++ pages/headerFooter/headerAdmin.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../../assets/css/headerAdmin.css">
+</head>
+<header>
+    <nav class="bg-color-white flex-align-items-center fs-1-3vw">
+        <a href="#home" class="headerImage">
+            <img src="../../assets/img/OGTOL.png" alt="OGTOLLogo" height="75vw" width="30%">
+        </a>
+        <div class="fs-1-3vw flex-justify-content-end logoutButtonBox">
+            <button class="logoutButton"><a href="../../index.php">Logout</a></button>
+        </div>
+    </nav>
+</header>
+</html>

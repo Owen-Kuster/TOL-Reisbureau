5253a0587c4e9e565e4ca772154349ea578bc941
Owen Kuster <owenkuster08@gmail.com>
2026-05-19 11:17:26 +0200

    Header fix (correctly include)
    Included correctly + added includes for the footer and homepage.

--- index.php
+++ index.php
@@ -1,2 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="stylesheet" href="assets/css/headerCss.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
 <?php
 include("pages/headerFooter/header.php"); 
+include("pages/homePage/home.php");
+include("pages/headerFooter/footer.php");
+?>
+</body>
+</html> 
--- pages/headerFooter/header.php
+++ pages/headerFooter/header.php
@@ -1,18 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <link rel="stylesheet" href="../../../assets/css/headerCss.css">
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-    <header>
+<header>
         <nav>
             <a href="#home" class="headerImage">
-                <img src="../../../assets/img/OGTOL.png" alt="" height="75vw" width="30%">
+                <img src="../../assets/img/OGTOL.png" alt="OGTOLLogo" height="75vw" width="30%">
             </a>
             <div class="headerNav">
                 <a href="#home">Home</a>
@@ -21,8 +10,7 @@
                 <a href="#contact">Contact</a>
             </div>
             <div class="headerProfile">
-                <img src="../../../assets/img/ProfilePictureNoLogin.png" alt="" height="30vw" width="30%">
+                <img src="../../assets/img/ProfilePictureNoLogin.png" alt="profilePicture" height="30vw" width="30%">
             </div>
         </nav>
     </header>
-</body>

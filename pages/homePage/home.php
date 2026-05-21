721f4e628f3badc21652426fdfa1cf2576168e74
Tygo00 <tygodiks@gmail.com>
2026-05-21 14:04:49 +0200

    Search home
    start search bar home screen

--- pages/homePage/home.php
+++ pages/homePage/home.php
@@ -10,8 +10,45 @@
         <main>
             <div class="container1">
                 <img src="../../assets/img/homeScreenImg.png" alt="" width="100%">
-                <div class="search-box">
-
+                <div class="travel-search-box">
+                    <div class="departure-box-search">
+                        <h3>Departure</h3>
+                        <div class="departure">
+                            <img src="../../assets/img/pinPointer.png" alt="" height="20px" width="20px">
+                        </div>   
+                    </div>
+                    <div class="destination-box-search">
+                        <h3>Destination</h3>
+                        <div class="destination">
+                            <img src="../../assets/img/pinPointer.png" alt="" height="20px" width="20px">
+                        </div>   
+                    </div>
+                    <div class="hotel-box-search">
+                        <h3>Hotel</h3>  
+                        <div class="hotel">
+                            <img src="../../assets/img/pointerDown.png" alt="">
+                        </div>    
+                    </div>
+                    <div class="departure-date-box-search">
+                        <h3>Departure date</h3>
+                        <div class="departure-date">
+                            <img src="../../assets/img/calender.png" alt="" height="20px" width="20px">
+                        </div>      
+                    </div>
+                    <div class="return-date-box-search">
+                        <h3>return date</h3>   
+                        <div class="return-date">
+                            <img src="../../assets/img/calender.png" alt="" height="20px" width="20px">
+                        </div>   
+                    </div>
+                    <div class="people-box-search">
+                        <h3>people</h3>   
+                        <div class="people"><img src="../../assets/img/people.png" alt="" height="13px" width="30px"></div>   
+                    </div>
+                    <div class="box-search">
+                        <h4>Search</h4>   
+                        <img src="../../assets/img/" alt="">  
+                    </div>
                 </div>
             </div>
 

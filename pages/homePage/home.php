fdb03c4af1f60437827cc7d5b9e765d0b6d2b459
Tygo00 <tygodiks@gmail.com>
2026-05-22 13:49:23 +0200

    landing page
    landing page search fixes

--- pages/homePage/home.php
+++ pages/homePage/home.php
@@ -8,8 +8,7 @@
     </head>
     <body>
         <main>
-            <div class="container1">
-                <img src="../../assets/img/homeScreenImg.png" alt="" width="100%">
+            <section class="landingspagina">
                 <div class="travel-search-box">
                     <div class="departure-box-search">
                         <h3>Departure</h3>
@@ -26,7 +25,8 @@
                     <div class="hotel-box-search">
                         <h3>Hotel</h3>  
                         <div class="hotel">
-                            <img src="../../assets/img/pointerDown.png" alt="">
+                            <h4>Yes</h4>
+                            <img src="../../assets/img/pointerDown.png" alt="" height="20px" width="20px">
                         </div>    
                     </div>
                     <div class="departure-date-box-search">
@@ -36,13 +36,13 @@
                         </div>      
                     </div>
                     <div class="return-date-box-search">
-                        <h3>return date</h3>   
+                        <h3>Return date</h3>   
                         <div class="return-date">
                             <img src="../../assets/img/calender.png" alt="" height="20px" width="20px">
                         </div>   
                     </div>
                     <div class="people-box-search">
-                        <h3>people</h3>   
+                        <h3>People</h3>   
                         <div class="people"><img src="../../assets/img/people.png" alt="" height="13px" width="30px"></div>   
                     </div>
                     <div class="box-search">
@@ -50,7 +50,7 @@
                         <img src="../../assets/img/" alt="">  
                     </div>
                 </div>
-            </div>
+            </section>
 
             <div class="cointainer2 test">
                 

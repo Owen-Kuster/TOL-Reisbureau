7b7f0d1ab660dbf8e483340db3b672f22fb4394c
LauraPos <1214217@student.roc-nijmegen.nl>
2026-05-28 14:13:17 +0200

    EOD

--- pages/bookPage/bookPageOutward.php
+++ pages/bookPage/bookPageOutward.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,6 +8,7 @@
     <link rel="stylesheet" href="../../assets/css/bookPageOutward.css">
     <?php include '../headerFooter/header.php' ?>
 </head>
+
 <body>
     <section class="outward">
         <div class="title">
@@ -16,8 +18,49 @@
 
         </div>
         <div class="placeholder">
-
+            <div class="bubble01">
+                <div class="bubble011">
+                    <div>
+                        <img src="../../img/Outward-Inward/departure.png" alt="departure">
+                    </div>
+                    <div>
+                        <img src="../../img/Outward-Inward/arrival.png" alt="arrival">
+                    </div>
+                </div>
+                <div class="bubble011">
+                    <div>
+                        <div> DTime
+                            <?php //php fill 
+                            ?>
+                        </div>
+                        <div> DPlace
+                            <?php //php fill 
+                            ?>
+                        </div>
+                    </div>
+                    <div>
+                        <div> ATime
+                            <?php //php fill 
+                            ?>
+                        </div>
+                        <div> APlace
+                            <?php //php fill 
+                            ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="bookNow">
+                <div>
+                    <a href="">ß</a>
+                </div>
+                <div>
+                    <?php //php fill 
+                    ?>
+                </div>
+            </div>
         </div>
     </section>
 </body>
+
 </html>

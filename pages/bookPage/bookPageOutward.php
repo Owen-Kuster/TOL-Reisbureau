968c273e8e0a3458ce7190a0fec099d8b2551100
LauraPos <1214217@student.roc-nijmegen.nl>
2026-05-28 12:46:59 +0200

    bookPageOutward
    Started with the CSS/HTML for the page bookPageOutward

---
+++ pages/bookPage/bookPageOutward.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Outward Journey</title>
+    <link rel="stylesheet" href="../../assets/css/bookPageOutward.css">
+    <?php include '../headerFooter/header.php' ?>
+</head>
+<body>
+    <section class="outward">
+        <div class="title">
+            <h1>Outward Journey</h1>
+        </div>
+        <div class="searchBar">
+
+        </div>
+        <div class="placeholder">
+
+        </div>
+    </section>
+</body>
+</html>

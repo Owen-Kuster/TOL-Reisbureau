36036068a41307f386b1337f32aed873bf4ea563
LauraPos <1214217@student.roc-nijmegen.nl>
2026-06-01 14:11:38 +0200

    Outward page finished
    Outward page finished HTML/CSS only Searchbar needs to be changed

--- pages/bookPage/bookPageOutward.php
+++ pages/bookPage/bookPageOutward.php
@@ -12,54 +12,98 @@
 <body>
     <section class="outward">
         <div class="title">
-            <h1>Outward Journey</h1>
+            <h1>Outward journey</h1>
         </div>
-        <div class="searchBar">
 
+        <div class="searchBar">
+            <form class="travel-search-box" action="" method="GET">
+
+                <div class="search-field">
+                    <label class="label">Departure</label>
+                    <div class="field-input">
+                        <img src="../../assets/img/pinPointer.png" alt="" height="20px" width="20px">
+                        <input type="text" name="departure" placeholder="City...">
+                    </div>
+                </div>
+
+                <div class="search-field">
+                    <label class="label">Destination</label>
+                    <div class="field-input">
+                        <img src="../../assets/img/pinPointer.png" alt="" height="20px" width="20px">
+                        <input type="text" name="destination" placeholder="City...">
+                    </div>
+                </div>
+
+                <div class="search-field">
+                    <label class="label">Hotel</label>
+                    <div class="field-input">
+                        <select name="hotel">
+                            <option value="yes">Yes</option>
+                            <option value="no">No</option>
+                        </select>
+
+                    </div>
+                </div>
+
+                <div class="search-field">
+                    <label class="label">Departure date</label>
+                    <div class="field-input">
+                        <img src="../../assets/img/calender.png" alt="" height="20px" width="20px">
+                        <input type="text" name="departure_date" placeholder="dd/mm/yyyy">
+                    </div>
+                </div>
+
+                <div class="search-field">
+                    <label class="label">Return date</label>
+                    <div class="field-input">
+                        <img src="../../assets/img/calender.png" alt="" height="20px" width="20px">
+                        <input type="text" name="return_date" placeholder="dd/mm/yyyy">
+                    </div>
+                </div>
+
+                <div class="search-field">
+                    <label class="label">People</label>
+                    <div class="field-input">
+                        <img src="../../assets/img/people.png" alt="" height="17px" width="40px">
+                        <input type="number" name="people" min="1" max="99" placeholder="1">
+                    </div>
+                </div>
+            </form>
         </div>
+
+        <!-- Vlucht kaart -->
         <div class="placeholder">
             <div class="bubble01">
-                <div class="bubble011">
-                    <div>
+                <div class="bubble011 icons-row">
+                    <div class="flight-icon">
                         <img src="../../img/Outward-Inward/departure.png" alt="departure">
                     </div>
-                    <div>
+                    <div class="flight-duration">
+                        <span>0lh 45m <!-- php fill --></span>
+                    </div>
+                    <div class="flight-icon">
                         <img src="../../img/Outward-Inward/arrival.png" alt="arrival">
                     </div>
                 </div>
-                <div class="bubble011">
-                    <div>
-                        <div> DTime
-                            <?php //php fill 
-                            ?>
-                        </div>
-                        <div> DPlace
-                            <?php //php fill 
-                            ?>
-                        </div>
+
+                <div class="bubble011 times-row">
+                    <div class="flight-point">
+                        <div class="time">06:45 <!-- php fill --></div>
+                        <div class="place">AMS <!-- php fill --></div>
                     </div>
-                    <div>
-                        <div> ATime
-                            <?php //php fill 
-                            ?>
-                        </div>
-                        <div> APlace
-                            <?php //php fill 
-                            ?>
-                        </div>
+                    <div class="flight-point right">
+                        <div class="time">08:30 <!-- php fill --></div>
+                        <div class="place">BCN <!-- php fill --></div>
                     </div>
                 </div>
             </div>
+
             <div class="bookNow">
-                <div>
-                    <a href="">ß</a>
-                </div>
-                <div>
-                    <?php //php fill 
-                    ?>
-                </div>
+                <a href="#" class="bookBtn">BOOK HERE</a>
+                <div class="seats">150 plaatsen over <!-- php fill --></div>
             </div>
         </div>
+
     </section>
 </body>
 

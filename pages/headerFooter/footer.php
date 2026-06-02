10263c439e690db95390b6e074a280528afefdad
Owen Kuster <owenkuster08@gmail.com>
2026-06-02 11:54:24 +0200

    header and footer redirections fix V1
    made the buttons work to other pages

--- pages/headerFooter/footer.php
+++ pages/headerFooter/footer.php
@@ -1,21 +1,21 @@
 <link rel="stylesheet" href="../../assets/css/footerCss.css">
 <header>
     <nav>
-        <a href="#home" class="footerImage">
+        <a href="../../pages/homePage/home.php" class="footerImage">
             <img src="../../assets/img/SimpelTOL.png" alt="SimpleTOLLogo" height="110vh" width="240vw">
         </a>
         <div class="footerNav">
-            <a href="#home">Home</a>
-            <a href="#book">Book</a>
-            <a href="#about">About us</a>
-            <a href="#contact">Contact</a>
+            <a href="../../pages/homePage/home.php">Home</a>
+            <a href="../../pages/bookPage/bookPageInward.php">Book</a>
+            <a href="../../pages/aboutUsPage/aboutUs.php">About us</a>
+            <a href="../../pages/contactPage/contact.php">Contact</a>
         </div>
         <div class="rules">
-            <a href="#generalRules.php">
+            <a href="../../pages/privacyGeneralPage/generalRules.php">
                 <h5 class="font-color-grey">©General Rules</h5>
             </a>
 
-            <a href="#generalRules.php">
+            <a href="../../pages/privacyGeneralPage/privacyPolicy.php">
                 <h5 class="font-color-grey">©Privacy Policy</h5>
             </a>
         </div>

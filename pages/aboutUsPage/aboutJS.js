7f768e05aa0cca552965f3f42bd697855d20bc53
LauraPos <1214217@student.roc-nijmegen.nl>
2026-06-04 13:35:35 +0200

    JS about us
    Started with JS in the about us page

---
+++ pages/aboutUsPage/aboutJS.js
@@ -0,0 +1,10 @@
+<script>
+    function openPopup(project) {
+      document.getElementById("popup-" + project).classList.remove("hidden");
+    }
+
+    function closePopup(project) {
+      document.getElementById("popup-" + project).classList.add("hidden");
+    }
+
+  </script>

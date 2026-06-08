efe62a46b84a89a8387a6022d8bf6286f4c0bad5
Owen Kuster <owenkuster08@gmail.com>
2026-06-08 15:49:35 +0200

    read.php + fix conn.php
    Made the read.php include all the 6 tables from our database (tested)

---
+++ dbcalls/crud/Read/read.php
@@ -0,0 +1,27 @@
+<?php
+
+//accommodations
+$stmt = $conn->query("SELECT * FROM accommodations");
+$accommodations = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+//contact
+$stmt = $conn->query("SELECT * FROM contact");
+$contact = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+//flights
+$stmt = $conn->query("SELECT * FROM flights");
+$flights = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+//review
+$stmt = $conn->query("SELECT * FROM review");
+$review = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+//trip
+$stmt = $conn->query("SELECT * FROM trip");
+$trip = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+//users
+$stmt = $conn->query("SELECT * FROM users");
+$users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+// echo"connected";

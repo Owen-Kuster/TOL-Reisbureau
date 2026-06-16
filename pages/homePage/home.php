bd0c7711f21a5299479457c2ce3e84e326353655
Tygo00 <tygodiks@gmail.com>
2026-06-16 10:29:51 +0200

    Sale home page
    finished sale section on home page

--- pages/homePage/home.php
+++ pages/homePage/home.php
@@ -1,6 +1,6 @@
 <?php
-include("dbcalls/connection/connection.php");
-include("dbcalls/crud/Read/read.php");
+include("../dbcalls/connection/connection.php");
+include("../dbcalls/crud/Read/read.php");
 ?>
 
 <!DOCTYPE html>
@@ -29,10 +29,22 @@ include("dbcalls/crud/Read/read.php");
                         <img src="../../assets/img/pinPointer.png" alt="" height="20px" width="20px">
                         <select name="departure">
                             <label>City...</label>
-                            <option value="">Amsterdam</option>
-                            <option value="">Porto</option>
-                            <option value="">Athens</option>
-                            <option value="">Barcalona</option>
+                            <option value="">
+                                <?php
+                                ?>
+                            </option>
+                            <option value="">
+                                <?php
+                                ?>
+                            </option>
+                            <option value="">
+                                <?php
+                                ?>
+                            </option>
+                            <option value="">
+                                <?php
+                                ?>
+                            </option>
                         </select>
                     </div>
                 </div>
@@ -43,10 +55,22 @@ include("dbcalls/crud/Read/read.php");
                         <img src="../../assets/img/pinPointer.png" alt="" height="20px" width="20px">
                         <select name="destination">
                             <label>City...</label>
-                            <option value="">Amsterdam</option>
-                            <option value="">Porto</option>
-                            <option value="">Athens</option>
-                            <option value="">Barcalona</option>
+                            <option value="">
+                                <?php
+                                ?>
+                            </option>
+                            <option value="">
+                                <?php
+                                ?>
+                            </option>
+                            <option value="">
+                                <?php
+                                ?>
+                            </option>
+                            <option value="">
+                                <?php
+                                ?>
+                            </option>
                         </select>
                     </div>
                 </div>
@@ -142,21 +166,65 @@ include("dbcalls/crud/Read/read.php");
 
             <div class="aanbiedingen-row">
                 <div class="aanbieding-box">
-                    
+                    <img class="aanbieding-img" src="../../assets/img/franceImgOne.png" alt="Hotel Cannes">
+                    <div class="aanbieding-info">
+                        <h3 class="aanbieding-naam">Family Staybridge Suites Cannes Centre - Frankrijk</h3>
+                        <div class="aanbieding-prijs">
+                            <p class="oude-prijs"><s>€299,-</s></p>
+                            <p class="nieuwe-prijs">€179,-</p>
+                        </div>
+                        <div class="aanbieding-kenmerken">
+                            <span>Toegang tot strand +</span>
+                            <span>Oplaadstation EV</span>
+                        </div>
+                    </div>
                 </div>
 
                 <div class="aanbieding-box">
-                    
+                    <img class="aanbieding-img" src="../../assets/img/spainImgTwo.png" alt="Hotel Spanje">
+                    <div class="aanbieding-info">
+                        <h3 class="aanbieding-naam">Canfranc Estación, a Royal Hideaway Hotel Family - Spanje</h3>
+                        <div class="aanbieding-prijs">
+                            <p class="oude-prijs"><s>€499,-</s></p>
+                            <p class="nieuwe-prijs">€369,-</p>
+                        </div>
+                        <div class="aanbieding-kenmerken">
+                            <span>Breakfast +</span>
+                            <span>Parking space</span>
+                        </div>
+                    </div>
                 </div>
             </div>
 
             <div class="aanbiedingen-row">
                 <div class="aanbieding-box">
-                    
+                    <img class="aanbieding-img" src="../../assets/img/greeceImgThree.png" alt="Hotel Griekenland">
+                    <div class="aanbieding-info">
+                        <h3 class="aanbieding-naam">Tholos Resort Family - Griekenland</h3>
+                        <div class="aanbieding-prijs">
+                            <p class="oude-prijs"><s>€399,-</s></p>
+                            <p class="nieuwe-prijs">€299,-</p>
+                        </div>
+                        <div class="aanbieding-kenmerken">
+                            <span>Toegang tot strand +</span>
+                            <span>Breakfast</span>
+                        </div>
+                    </div>
                 </div>
 
                 <div class="aanbieding-box">
-                    
+                    <img class="aanbieding-img" src="../../assets/img/spainImgFour.png" alt="Hotel Granada">
+                    <div class="aanbieding-info">
+                        <h3 class="aanbieding-naam">Hotel Granada Family Palace, Affiliated by Meliá - Spanje</h3>
+                        <div class="aanbieding-prijs">
+                            <p class="oude-prijs"><s>€399,-</s></p>
+                            <p class="nieuwe-prijs">€349,-</p>
+                        </div>
+                        <div class="aanbieding-kenmerken">
+                            <span>Toegang tot strand +</span>
+                            <span>Parking space</span>
+                        </div>
+                    </div>
                 </div>
             </div>
 
@@ -171,6 +239,14 @@ include("dbcalls/crud/Read/read.php");
         include(__DIR__ . "/../headerFooter/footer.php");
         ?>
     </footer>
+
+    <script>
+        setTimeout(function () {
+            const box = document.querySelector('.travel-search-box');
+            if (box) box.classList.add('visible');
+        }, 100);
+    </script>
+
 </body>
 
 </html>
